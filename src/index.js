2e2413739461db6325e5c1e70453b962cda978f2
Sohaib Arshid <sohaibali1277@gmail.com>
2026-02-22 12:09:29 +0500

    setup express server and install cors cookieparser and use middleware

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import { connectdb } from "./db/index.js";
 import dotenv from "dotenv"
-dotenv.config({path : "./env"})
+dotenv.config({ path: "./env" })
+import app from "./app.js"
 
 connectdb()
+    .then(() => {
+        const server = app.listen(process.env.PORT, () => {
+            console.log(`server is listing on ${process.env.PORT}`);
+        })
+
+        server.on("error", (error) => {
+            console.log("Error in port", error);
+        })
+    })
+    .catch((error) => {
+        console.log("Error in server connection !!", error);
+    })
