c83e79301b8bb3c880b26d5262278f264f90abb3
Sohaib Arshid <sohaibali1277@gmail.com>
2026-03-03 16:56:31 +0500

    define user schema

---
+++ src/models/user.models.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    bio: {
+        type: String,
+        trim: true,
+    },
+    skills: {
+        type: [String],
+        required: true,
+    },
+    profilePic: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true
+});
+
+// Pre-save hook to hash password
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+// Method to check if password is correct
+UserSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+// Method to generate Access Token
+UserSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        name: this.name
+    }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    });
+};
+
+// Method to generate Refresh Token
+UserSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id,
+    }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    });
+};
+
+const User = mongoose.model("User", UserSchema);
+export default User;
