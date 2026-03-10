b9bb788a25e890e77a038e9b904d7a40d40553c1
Sohaib Arshid <sohaibali1277@gmail.com>
2026-03-11 00:28:53 +0500

    implement fetch follower code

--- src/controllers/follow/getFollower.js
+++ src/controllers/follow/getFollower.js
@@ -1,10 +1,23 @@
-import Follow from "../../models/follow.models";
-import ApiError from "../../utils/ApiError.js";
-import ApiResponse from "../../utils/ApiResponse.js";
-import asyncHandler from "../../utils/asyncHandler.js";
+import Follow from "../../models/follow.models.js"
+import ApiError from "../../utils/ApiError.js"
+import ApiResponse from "../../utils/ApiResponse.js"
+import asyncHandler from "../../utils/asyncHandler.js"
 
-const getFollower = asyncHandler(async (req , res)=>{
-    
+const getFollowers = asyncHandler(async (req, res) => {
+    const userId = req.user._id
+
+    const followers = await Follow.find({
+        following: userId,
+        status: "accepted"
+    })
+
+    if (followers.length === 0) {
+        throw new ApiError(404, "No followers found")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, followers, "Followers fetched successfully")
+    )
 })
 
-export {getFollower}
+export { getFollowers }
